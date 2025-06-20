bd86691fe9df115180a65d0964829273f7c86110
Archimentor <128781156+Archimentor@users.noreply.github.com>
2025-06-20 15:46:12 +0900

    Refactor bulk data deletion

--- _establishment/list/data_delete.php
+++ _establishment/list/data_delete.php
@@ -24,41 +24,69 @@ while($row = sql_fetch_array($result)) {
     sql_query("DELETE FROM {$g5['board_file_table']} WHERE seq = '{$row['seq']}'");
 }
 
-// 노임 인부정보 첨부파일 삭제
-$sql = "select ne_file1, ne_file2, ne_file3 from {$none['est_noim']} where nw_code = '{$nw_code}' and ne_date = '{$date}' and ne_type = '1'";
-$rst = sql_query($sql);
-while($row = sql_fetch_array($rst)) {
-    foreach(array('ne_file1','ne_file2','ne_file3') as $f) {
-        if($row[$f]) {
-            $fp = NONE_PATH."/_data/noim/{$nw_code}/".$row[$f];
-            if(file_exists($fp)) @unlink($fp);
-        }
-    }
-}
-sql_query("update {$none['est_noim']} set ne_file1='', ne_file2='', ne_file3='' where nw_code = '{$nw_code}' and ne_date = '{$date}' and ne_type='1'");
+// 첨부파일이 있는 하위 테이블 정의
+$tables = [
+    [
+        'table' => $none['est_noim'],
+        'cols'  => ['ne_file1','ne_file2','ne_file3'],
+        'dir'   => 'noim',
+        'where' => "nw_code='{$nw_code}' and ne_date='{$date}' and ne_type='1'"
+    ],
+    [
+        'table' => $none['est_noim'],
+        'cols'  => ['ne_file1','ne_file2'],
+        'dir'   => 'noim2',
+        'where' => "nw_code='{$nw_code}' and ne_date='{$date}' and ne_type='2'"
+    ],
+    [
+        'table' => $none['est_material'],
+        'cols'  => ['ne_file1','ne_file2','ne_file3'],
+        'dir'   => 'material',
+        'where' => "nw_code='{$nw_code}' and ne_date='{$date}'"
+    ],
+    [
+        'table' => $none['est_equipment'],
+        'cols'  => ['ne_file1','ne_file2','ne_file3'],
+        'dir'   => 'equipment',
+        'where' => "nw_code='{$nw_code}' and ne_date='{$date}'"
+    ],
+    [
+        'table' => $none['est_etc'],
+        'cols'  => ['ne_file1','ne_file2','ne_file3'],
+        'dir'   => 'etc',
+        'where' => "nw_code='{$nw_code}' and ne_date='{$date}'"
+    ],
+];
 
-// 노임 용역회사 첨부파일 삭제
-$sql = "select seq, ne_file1, ne_file2 from {$none['est_noim']} where nw_code = '{$nw_code}' and ne_date = '{$date}' and ne_type = '2'";
-$rst = sql_query($sql);
-while($row = sql_fetch_array($rst)) {
-    foreach(array('ne_file1','ne_file2') as $f) {
-        if($row[$f]) {
-            $fp = NONE_PATH."/_data/noim2/{$nw_code}/".$row[$f];
+foreach($tables as $info) {
+    $col_list = implode(',', $info['cols']);
+    $sql  = "select {$col_list} from {$info['table']} where {$info['where']}";
+    $rst  = sql_query($sql);
+    while($row = sql_fetch_array($rst)) {
+        foreach($info['cols'] as $c) {
+            if(empty($row[$c])) continue;
+            $fp = NONE_PATH."/_data/{$info['dir']}/{$nw_code}/".$row[$c];
             if(file_exists($fp)) @unlink($fp);
         }
     }
-    $file = sql_fetch("select ne_file3, ne_file4 from none_est_noim_file where noim_seq = '{$row['seq']}' and ne_date = '{$date}'");
-    if($file) {
-        foreach(array('ne_file3','ne_file4') as $f) {
-            if($file[$f]) {
-                $fp = NONE_PATH."/_data/noim2/{$nw_code}/".$file[$f];
-                if(file_exists($fp)) @unlink($fp);
-            }
-        }
-        sql_query("delete from none_est_noim_file where noim_seq = '{$row['seq']}' and ne_date = '{$date}'");
-    }
+    $set = implode("='', ", $info['cols'])."=''";
+    sql_query("update {$info['table']} set {$set} where {$info['where']}");
 }
-sql_query("update {$none['est_noim']} set ne_file1='', ne_file2='' where nw_code = '{$nw_code}' and ne_date = '{$date}' and ne_type='2'");
+
+// 노임 용역회사 추가 파일 삭제 처리
+$sql = "select seq from {$none['est_noim']} where nw_code = '{$nw_code}' and ne_date = '{$date}' and ne_type='2'";
+$rst = sql_query($sql);
+while($row = sql_fetch_array($rst)) {
+    $file = sql_fetch("select ne_file3, ne_file4 from none_est_noim_file where noim_seq = '{$row['seq']}' and ne_date = '{$date}'");
+    if(!$file) continue;
+    foreach(['ne_file3','ne_file4'] as $f) {
+        if($file[$f]) {
+            $fp = NONE_PATH."/_data/noim2/{$nw_code}/".$file[$f];
+            if(file_exists($fp)) @unlink($fp);
+        }
+    }
+    sql_query("delete from none_est_noim_file where noim_seq = '{$row['seq']}' and ne_date = '{$date}'");
+}
 
 alert('첨부파일이 모두 삭제되었습니다.', './menu1_list.php?date=' . urlencode($date));
 ?>
